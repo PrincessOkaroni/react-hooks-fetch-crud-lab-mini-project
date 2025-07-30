bd2e7ae8a06514f0a0608ef1db16bfdca9fa1294
Okaroni Purity <nekesapurity20@gmail.com>
2025-07-30 18:20:03 +0300

    lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((questions) => {
+        setQuestions(questions);
+      });
+  }, []);
+  function handleAddForm(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+
+  function handleDeleteQuestion(deletedQuestion) {
+    setQuestions(questions.filter((question) => question.id !== deletedQuestion.id));
+  }
+
+  function handleUpdateQuestion(updatedQuestion) {
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === updatedQuestion.id) {
+        return updatedQuestion;
+      }
+      return question;
+    });
+    setQuestions(updatedQuestions);
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddForm={handleAddForm}/> 
+      : <QuestionList questions={questions}  onDeleteQuestion={handleDeleteQuestion} onUpdateQuestion={handleUpdateQuestion}/>}
     </main>
   );
 }
